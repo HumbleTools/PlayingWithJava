4e8ea9de36c03280259a2a3f9dbde13f3e8e1281
louis madeuf <louis.madeuf@gfi.fr>
2015-04-29 13:51:28 +0200

    twentyonesticks continue

--- src/TwentyOneSticks.java
+++ src/TwentyOneSticks.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * This is a fun, simple and known game made in a simple console version.
  * 
@@ -6,11 +8,32 @@
  */
 public class TwentyOneSticks {
 
+	final static Scanner scanner = new Scanner(System.in);
+	
 	/**
 	 * Where all the fun starts !
 	 * @param args : the arguments passed, although we don't need them here.
 	 */
 	public static void main(final String[] args){
+		System.out.println("TWENTY ONE STICKS GAME !");
+		System.out.println("Welcome to the twenty-one sticks game in JAVA.");
 		
+		if(isPlayerFirst()){
+			System.out.println("OK, you will play first.");
+			
+		}else{
+			System.out.println("OK, the computer will play first.");
+		}
+	}
+
+	private static boolean isPlayerFirst() {
+		System.out.println("Do you wish to start first ? (y/n)");
+		String userInput = scanner.nextLine();
+		while(!"y".equalsIgnoreCase(userInput) && !"n".equalsIgnoreCase(userInput)){
+			System.out.println("Wrong input. Expecting one of these : y Y n N");
+			System.out.println("Do you wish to start first ? (y/n)");
+			userInput = scanner.nextLine();
+		}
+		return "y".equalsIgnoreCase(userInput);
 	}
 }
