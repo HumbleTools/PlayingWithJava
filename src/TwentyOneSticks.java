f90a11fb225b29523b08ad00f191c9d7b42c96b7
louis madeuf <louis.madeuf@gfi.fr>
2015-04-29 17:43:57 +0200

    twentyonesticks is almost done, only AI is missing

--- src/TwentyOneSticks.java
+++ src/TwentyOneSticks.java
@@ -8,6 +8,16 @@ import java.util.Scanner;
  */
 public class TwentyOneSticks {
 
+	private static final String STICK = " |";
+	private static final String COMPUTER_FIRST = "OK, the computer will play first.";
+	private static final String PLAYER_FIRST = "OK, you will play first.";
+	private static final String PLAYER_WON = "The computer removed the last stick. You won ! :)";
+	private static final String PLAYER_LOST = "You removed the last stick. You lost ! :(";
+	private static final String THE_COMPUTER_REMOVED_X_STICKS = "The computer removed %s stick(s).";
+	private static final String YOU_REMOVED_X_STICKS = "You removed %s stick(s).";
+	private static final String WRONG_INPUT_EXPECTING_1_2_OR_3 = "Wrong input. Expecting 1, 2 or 3.";
+	private static final String ASK_ONE_TWO_OR_THREE_STICKS = "Do you want to remove one, two or three sticks ? (1/2/3)";
+	private static final String THERE_ARE_X_STICKS_LEFT = "There are %s sticks left :";
 	private static final String RULE_3 = "Rule 3 : If you take the last stick, you loose !";
 	private static final String RULE_2 = "Rule 2 : You can take one, two or three sticks at each turn.";
 	private static final String RULE_1 = "Rule 1 : There are 21 sticks.";
@@ -28,13 +38,11 @@ public class TwentyOneSticks {
 		System.out.println(WELCOME_PHRASE);
 		printRules();
 		if(isPlayerFirst()){
-			System.out.println("OK, you will play first.");
-			System.out.println();
-			runGame(true);
+			System.out.println(PLAYER_FIRST);
+			runGameTurns(true);
 		}else{
-			System.out.println("OK, the computer will play first.");
-			System.out.println();
-			runGame(false);
+			System.out.println(COMPUTER_FIRST);
+			runGameTurns(false);
 		}
 	}
 
@@ -55,7 +63,7 @@ public class TwentyOneSticks {
 	 */
 	private static boolean isPlayerFirst() {
 		System.out.println(START_QUESTION_Y_N);
-		String userInput = scanner.nextLine();
+		String userInput = scanner.nextLine().trim();
 		while(!"y".equalsIgnoreCase(userInput) && !"n".equalsIgnoreCase(userInput)){
 			System.out.println(WRONG_INPUT_Y_N);
 			System.out.println(START_QUESTION_Y_N);
@@ -65,15 +73,93 @@ public class TwentyOneSticks {
 	}
 	
 	/**
-	 * Runs the game until the end. When there are 0 sticks or less, the game ends.
+	 * Runs the game turns until the end. When there are 0 sticks or less, the game ends.
 	 */
-	private static void runGame(final boolean isPlayerFirst) {
-		while(stickCount>0){
-			if(isPlayerFirst){
-				
+	private static void runGameTurns(final boolean isPlayerFirst) {
+		boolean isPlayersTurn = isPlayerFirst;
+		boolean hasGameEnded = false;
+		while(!hasGameEnded){
+			drawSticks();
+			removeSticks(isPlayersTurn);
+			hasGameEnded = hasGameEnded(isPlayersTurn);
+			isPlayersTurn = !isPlayersTurn;
+		}
+	}
+
+	/**
+	 * Determines if the game has ended. If it has, it outputs the results.
+	 */
+	private static boolean hasGameEnded(final boolean isPlayersTurn) {
+		boolean hasGameEnded = false;
+		if(stickCount <= 0){
+			hasGameEnded = true;
+			if(isPlayersTurn){
+				System.out.println(PLAYER_LOST);
 			}else{
-				
+				System.out.println(PLAYER_WON);
 			}
+		}
+		return hasGameEnded;
+	}
+
+	/**
+	 * Removes a number of sticks equal to sticksToRemove and tells who removed them.
+	 * @param sticksToRemove : the number of sticks to remove.
+	 * @param isPlayersTurn : determines the author of the removal.
+	 */
+	private static void removeSticks(boolean isPlayersTurn) {
+		int sticksToRemove;
+		if(isPlayersTurn){
+			sticksToRemove = askForSticksToRemove();
+			stickCount -= sticksToRemove;
+			System.out.println(String.format(YOU_REMOVED_X_STICKS, sticksToRemove));
+		}else{
+			sticksToRemove = determineNumberOfSticksToRemove();
+			stickCount -= sticksToRemove;
+			System.out.println(String.format(THE_COMPUTER_REMOVED_X_STICKS, sticksToRemove));
+		}
+	}
+
+	/**
+	 * Determines the number of sticks the computer should remove.
+	 * @return int the number of sticks the computer will remove.
+	 */
+	private static int determineNumberOfSticksToRemove() {
+		System.out.println();
+		
+		// TODO implement AI here :o)
+		
+		return 2;
+	}
+
+	/**
+	 * Asks the player to enter a valid number of sticks : 1, 2 or 3.
+	 * @return int returns the first correctly entered number.
+	 */
+	private static int askForSticksToRemove() {
+		System.out.println();
+		System.out.println(ASK_ONE_TWO_OR_THREE_STICKS);
+		String userInput = scanner.nextLine().trim();
+		while(!"1".equals(userInput) && !"2".equals(userInput) && !"3".equals(userInput)){
+			System.out.println(WRONG_INPUT_EXPECTING_1_2_OR_3);
+			System.out.println(ASK_ONE_TWO_OR_THREE_STICKS);
+			userInput = scanner.nextLine().trim();
+		}
+		int sticksToRemove = Integer.parseInt(userInput);
+		if(sticksToRemove > stickCount){
+			sticksToRemove = stickCount;
+		}
+		return sticksToRemove;
+	}
+
+	/**
+	 * Draws the remaning sticks in the standard output.
+	 */
+	private static void drawSticks() {
+		System.out.println();
+		System.out.println(String.format(THERE_ARE_X_STICKS_LEFT, stickCount));
+		for(int i=1; i<=stickCount; i++){
+			System.out.print(STICK);
 		}
 	}
 	
