8d6ddb8d48a224ebaad879dccf558b702699b961
louis madeuf <louis.madeuf@gfi.fr>
2015-04-29 12:17:26 +0200

    init project

---
+++ src/TwentyOneSticks.java
@@ -0,0 +1,16 @@
+/**
+ * This is a fun, simple and known game made in a simple console version.
+ * 
+ * @author lmadeuf
+ *
+ */
+public class TwentyOneSticks {
+
+	/**
+	 * Where all the fun starts !
+	 * @param args : the arguments passed, although we don't need them here.
+	 */
+	public static void main(final String[] args){
+		
+	}
+}
